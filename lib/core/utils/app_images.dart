7a2a8a0f22c0ba4e41aaf5e9cd5d92f2b6d50d66
Ahmed7Shaban <ahmed791shaban@gmail.com>
2024-10-04 17:06:27 +0300

    done ui splash

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for appIconAppIcon
+  /// assets/appIcon/app_icon.png
+  static const String appIconAppIcon = "assets/appIcon/app_icon.png";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for imagesSplashIcon
+  /// assets/images/splash_icon.svg
+  static const String imagesSplashIcon = "assets/images/splash_icon.svg";
+
+  /// Assets for imagesSplashTop
+  /// assets/images/splash_top.svg
+  static const String imagesSplashTop = "assets/images/splash_top.svg";
+}
+
